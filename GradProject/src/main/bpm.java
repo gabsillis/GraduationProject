8c21ce1e3c1364b05dcb9c97dbb46372e76bd78b
Gianni <gianniabsillis@gmail.com>
2015-11-04 15:55:46 -0500

    Merging

--- GradProject/src/main/bpm.java
+++ GradProject/src/main/bpm.java
@@ -1,7 +1,4 @@
 package main;
-import java.io.IOException;
-
-import javax.sound.sampled.AudioInputStream;
 
 
 public class bpm {
@@ -9,22 +6,22 @@ public class bpm {
 		
 	public static double getBPM(int[][] samples,int channels,int frameLength,int frameSize,float sampleRate){
 
-		double highAverage = 0.0;
-		double lowAverage = 0.0;
-		// only doing for one channel for now
-		for(int i=0;i<frameLength; i=i+2){
-			highAverage = highAverage+samples[0][i];
-			lowAverage = lowAverage+samples[0][i+1];
-		}
-		highAverage = highAverage/(frameLength*frameSize);
-		lowAverage = lowAverage/(frameLength*frameSize);
-		double absoluteAverage = (Math.abs(highAverage)+Math.abs(lowAverage))/2;
+		double Average = 0.0;
 		double bpm = 0.0;
-		for(int j=0;j<60*sampleRate/1024;j = j+1024){// for 1 min of music
-			if((Math.abs(samples[0][j])+Math.abs(samples[0][j]))/2 > absoluteAverage*BEATTHRESHOLD){
-				bpm = bpm+1;
+		for(int k=0; k< channels; k++){
+			Average = 0.0;
+			for(int i=0;i<frameLength; i++){
+				Average = Average + samples[k][i];
+			}
+			Average = Average/frameLength;
+			double absoluteAverage = Average;
+
+			for(int j=0;j<60*sampleRate/1024;j = j+1024){// for 1 min of music
+				if(samples[k][j] > absoluteAverage*BEATTHRESHOLD){
+					bpm = bpm+1;
+				}
 			}
 		}
-		return bpm;
+		return bpm/channels; // because in the channels for loop i keep adding to the bpm of previous channel
 	}
 }
